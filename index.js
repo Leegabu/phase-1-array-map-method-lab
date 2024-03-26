14f58ff8aea21d98b53bc6c6846c2aa5ab232b35
Lee <leeirungu11313@gmail.com>
2024-03-26 22:22:40 +0300

    Add index.js

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    const words = tutorial.split(' ');
+    return words.map((word, index) => {
+      return index === 0 , word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
 }
+console.log(titleCased());
